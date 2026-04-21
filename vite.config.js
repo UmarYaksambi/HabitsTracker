b04dae081a5eb140dcde7ca7ec7dabb81250c3b7
Umar Yaksambi <umaryaksambi@gmail.com>
2026-04-22 00:35:40 +0530

    Feat: PWA compatibility

--- vite.config.js
+++ vite.config.js
@@ -25,6 +25,16 @@ export default defineConfig({
             type: 'image/svg+xml',
             purpose: 'any maskable',
           },
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
         ],
       },
       workbox: {
